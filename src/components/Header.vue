a1b012abb2c927cee45226437dbe965a630e1a3d
laveena95 <laveenalavee256@gmail.com>
2020-08-11 13:14:13 +0530

    views added

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-toolbar >
+      <v-toolbar-title>KICK Products</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only" >
+        <v-btn text>
+          <router-link to="/">HOME</router-link>
+        </v-btn>
+
+        <v-btn>
+          <router-link to="/about">ABOUT</router-link>
+        </v-btn>
+
+        <v-btn>
+          <router-link to="/contact">CONTACT</router-link>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
+import Menu from '../views/Menu.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,17 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: Menu
+  },
 ]
 
 const router = new VueRouter({
